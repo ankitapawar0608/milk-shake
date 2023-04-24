c6d706db2580c0138ce139906a0512935169d95f
ankitapawar0608 <kedarpawar775@gmail.com>
2023-04-24 13:32:15 +0530

    First commit

---
+++ src/MilkShake.js
@@ -0,0 +1,24 @@
+import './MilkShake.css'
+
+
+function MilkShake() {
+  return (
+   <>
+   <h1 className='heading'>MilkShakes 🥤</h1>
+   <div className='container'>
+
+  
+   <img src='https://images.pexels.com/photos/12365249/pexels-photo-12365249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='imgStyle' alt='milk'/>
+
+   <h1 className='title'> Oreo Milkshake</h1>
+   <h2 className='para'>This Oreo milkshake is easy to make, super kid-friendly, and delicious - a favorite on a hot day!
+
+</h2>
+<br/>
+
+   </div>
+   </>
+  );
+}
+
+export default MilkShake;
