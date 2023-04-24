5ac20d3610d6ec83b60ed12374bd9b022e7b7085
ankitapawar0608 <kedarpawar775@gmail.com>
2023-04-24 15:20:59 +0530

    Imgs added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import MilkShake from "./MilkShake";
+
+function App() {
+    return (
+        <>
+        
+        
+    )
+}
+
+export default App;
--- src/MilkShake.js
+++ src/MilkShake.js
@@ -2,15 +2,15 @@ import './MilkShake.css'
 
 
 
-function MilkShake() {
+function MilkShake({title,description,imgUrl}) {
   return (
     <>
-      <h1 className='heading'>MilkShakes 🥤</h1>
+     
       <div className='container'>
-        <img src='https://images.pexels.com/photos/12365249/pexels-photo-12365249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='imgStyle' alt='milk' />
+        <img src={imgUrl} className='imgStyle' alt='milk' />
 
-        <h1 className='title'> Oreo Milkshake</h1>
-        <h2 className='para'>This Oreo milkshake is easy to make, super kid-friendly, and delicious - a favorite on a hot day!  </h2>  <br />
+        <h1 className='title'> {title}</h1>
+        <h2 className='para'>{description} </h2>  <br />
 
       </div>
 
